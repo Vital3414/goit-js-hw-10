cd9e334d0e8653bd9c6d7da382a3ca4a6ac9dfc9
Vital3414 <qwer1234432148@gmail.com>
2023-04-18 12:07:27 +0300

    upd hw 10

---
+++ src/fetchCountries.js
@@ -0,0 +1,7 @@
+export function fetchCountries(query) {
+  const sQuery = query + '?fields=name,capital,population,flags,languages';
+
+  return fetch(`https://restcountries.com/v2/name/${sQuery}`).then(res =>
+    res.json()
+  );
+}
